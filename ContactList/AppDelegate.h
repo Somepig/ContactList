fa63e0013df3d37beba8a7270d756b2338f1e4cb
iMac <imac@iMactekiiMac.local>
2013-10-24 13:44:46 +0800

    Initial Commit

---
+++ ContactList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ContactList
+//
+//  Created by iMac on 13-10-24.
+//  Copyright (c) 2013年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ContactList/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ContactList
+//
+//  Created by iMac on 13-10-24.
+//  Copyright (c) 2013年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
